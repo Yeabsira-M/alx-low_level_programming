d1f9ee68604bd80c0306e37d22d18f64e1f51641
yeabsira <yeabsira190@gmail.com>
2022-12-12 13:23:07 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - returns alphabets bot loerw and upper c
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+
+	putchar('\n');
+	return (0);
+}
