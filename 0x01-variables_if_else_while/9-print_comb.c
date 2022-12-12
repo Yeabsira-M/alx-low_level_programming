ff7d4ed7e841eccdb43d6557d520234dcef8ccd7
yeabsira <yeabsira190@gmail.com>
2022-12-12 14:23:20 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int a = 0;
-       
-	while (a < 10)
-{
-	putchar(48 + a);
-	if (a != 9)
-	{
+	
+	 while (a < 10)
+	  {
+	    putchar(48 + a);
+	    if (a != 9)
+              	 {      
 			 putchar(',');
 			 putchar(' ');
 		 }
 		 a++;
-	 }
+	  }
 	 return (0);
 }
