70bbf9ef2554fd52095e68cf5086a2cac7f735c2
yeabsira <yeabsira190@gmail.com>
2022-12-12 14:02:48 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	 while (a < 10)
+	 {
+		 putchar(48 + a);
+		 if (a != 9)
+		 {
+			 putchar(',');
+			 putchar(' ');
+		 }
+		 a++;
+	 }
+}
