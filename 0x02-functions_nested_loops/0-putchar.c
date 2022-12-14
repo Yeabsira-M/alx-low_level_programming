5552748faaf46b0fbdf611932914b0d095813f37
yeabsira <yeabsira190@gmail.com>
2022-12-14 03:58:52 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include "main.h"                                      
 
-                                                       
-
 /**                                                    
  * main- - main block                              
  *
@@ -10,7 +8,7 @@
 
 int main(void)                                         
 
-{                                                      	        _putchar('_');
+{       _putchar('_');
 	_putchar('p'); 
 	_putchar('u');         
 	_putchar('t'); 
