cbd882e3437c9903e5a4659524a1e06699cbb448
yeabsira <yeabsira190@gmail.com>
2022-12-14 11:27:10 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower -checks for lower char
+ * @c: the char to be checked
+ * Return: 1 for lower char or 0 for anyting
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c<= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
