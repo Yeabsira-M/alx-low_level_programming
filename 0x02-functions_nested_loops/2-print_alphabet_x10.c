6e2405264db4158d99c93686ddcf2685ed1dc7e7
yeabsira <yeabsira190@gmail.com>
2022-12-14 08:56:56 -0800

    ./2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alpabet_x10 - alphabet 10x
+ * Return: void
+ */
+void print_alphabet_x10(void)
+
+{
+	char ch;
+	int j = 0;
+
+	while (j <= 9)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		j++;}
+}
