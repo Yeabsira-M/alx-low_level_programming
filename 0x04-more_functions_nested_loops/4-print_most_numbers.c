aa395bbe344c2936ec8d744f67eca3a94b62ab03
yeabsira <yeabsira190@gmail.com>
2022-12-16 14:18:04 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print bn 0 -9
+ * (2 or 4).
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
