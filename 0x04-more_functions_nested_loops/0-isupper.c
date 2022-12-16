f437b8dc6eb9d8a1b416bceadda99e33586ec455
yeabsira <yeabsira190@gmail.com>
2022-12-16 06:23:49 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upchase char
+ * @c:char to be checked
+ *
+ * Return: 1 if character is uppercase. 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File - main.h
+ * Author: Genius Villagers
+ * Desc: Header contains prototypes for all functions used in 0x03-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+#endif
