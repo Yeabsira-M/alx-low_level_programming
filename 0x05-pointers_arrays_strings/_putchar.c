02354aaa7e139acd9d16b6b13b15ad377c5f8e86
yeabsira <yeabsira190@gmail.com>
2022-12-19 02:39:03 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unstid.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: the char to be printed
+ *
+ * Return: sucess 1
+ * on error -1
+ */
+int _putchar9char c)
+{
+	return (write (1, &c, 1));
+}
