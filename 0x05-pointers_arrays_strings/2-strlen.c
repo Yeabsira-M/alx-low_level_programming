37ea5d2a845b212fa6a06329c1378079fe852d47
yeabsira <yeabsira190@gmail.com>
2022-12-19 02:11:33 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function to find length of string
+ *  @c: string pointer
+ * @length: string length
+ * Return: returns string length
+ */
+int _strlen(char *c)
+{
+	int length;
+
+	for ( ; *c != '\0\; c++)
+	{
+		length += 1;
+	}
+	return (l);
+}
