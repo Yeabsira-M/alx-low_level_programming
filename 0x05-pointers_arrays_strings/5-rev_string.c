f2b4fa3727ac59ec6b649449ab9aeb2814ab15e6
yeabsira <yeabsira190@gmail.com>
2022-12-19 10:08:30 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,12 +14,12 @@ void rev_string(char *s)
 		length1++;
 
 	length2 = length1 - 1
-		for (i = 0; i < length; i++)
+		for (i = 0; i < length1 / 2; i++)
 		{
 			"yabalx", "xlabay"
-				tmp = s[i];
+			tmp = s[i];
 			s[i] = s[length2];
 			s[length2] = tmp;
-			length -= 1;
+			length2 -= 1;
 		}
 }
